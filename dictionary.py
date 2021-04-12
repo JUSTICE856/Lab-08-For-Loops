472385552f0f19a6ad1d5b51a29e1b1947a28cd5
JUSTIC856 <justice.ndukwu@mail.citytech.cuny.edu>
2021-04-12 15:25:52 -0400

    Completed

--- dictionary.py
+++ dictionary.py
@@ -1,1 +1,12 @@
 myData = {"effective top tube length": 515, "seat tube length": 500, "seat tube angle": 74.4, "head tube angle": 70.5, "stack": 513, "reach": 367, "standover height": 755}
+mykeys = []
+myValues = []
+
+# iteration in parrallel
+for k, v in myData.items():
+    print("key: " + str(k) + ", value: " + str(v))
+    mykeys.append(k)
+    myValues.append(v)
+
+print("ALL KEYS: " + str(mykeys))
+print("ALL VALUES: " + str(myValues))
